a14fdd2fe41e2ca141b4469ce6f561ea92c25abf
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-04-02 12:59:07 -0300

    Aula02

--- src/main/java/br/com/gft/istemaleilao/service/Avaliador.java
+++ src/main/java/br/com/gft/istemaleilao/service/Avaliador.java
@@ -1,27 +1,52 @@
 
 package br.com.gft.istemaleilao.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.gft.istemaleilao.domain.Lance;
 import br.com.gft.istemaleilao.domain.Leilao;
 
 public class Avaliador {
-	
+
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	private List<Lance> maiores;
 
-	
-	public void avalia (Leilao leilao) {
-		for (Lance lance: leilao.getLances()) {
-			if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
-			if (lance.getValor()<menorDeTodos ) menorDeTodos = lance.getValor();
+	public void avalia(Leilao leilao) {
+		for (Lance lance : leilao.getLances()) {
+			if (lance.getValor() > maiorDeTodos)
+				maiorDeTodos = lance.getValor();
+			if (lance.getValor() < menorDeTodos)
+				menorDeTodos = lance.getValor();
 		}
+
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+
+			public int compare(Lance o1, Lance o2) {
+				if (o1.getValor() < o2.getValor())
+					return 1;
+				if (o1.getValor() > o2.getValor())
+					return -1;
+
+				return 0;
+			}
+		});
+
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
 	}
 
+	public List<Lance> getTresMaiores() {
+		return maiores;
+	}
 
 	public double getMaiorLance() {
 		return maiorDeTodos;
 	}
-	
+
 	public double getMenorLance() {
 		return menorDeTodos;
 	}
--- src/test/java/br/com/gft/istemaleilao/test/TesteDoAvaliador.java
+++ src/test/java/br/com/gft/istemaleilao/test/TesteDoAvaliador.java
@@ -2,6 +2,8 @@ package br.com.gft.istemaleilao.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.gft.istemaleilao.domain.Lance;
@@ -37,5 +39,80 @@ public class TesteDoAvaliador {
 		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
 	
 	}
+	
+	@Test
+	public void deveEntenderLancesEmOrdemDecrescente() {
+		
+		//cenario
+		Usuario joao = new Usuario("Joao");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
 
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+		
+		leilao.propoe(new Lance(joao,3000.0));
+		leilao.propoe(new Lance(jose,2000.0));
+		leilao.propoe(new Lance(maria, 1000.0));
+		
+		//acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		//validacao
+		double maiorEsperado = 3000;
+		double menorEsperado = 1000;
+		
+		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	
+	}
+	
+	@Test
+	public void deveEntenderLeilaoComApenasUmLance() {
+		
+		//cenario
+		Usuario joao = new Usuario("Joao");
+	
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+		
+		leilao.propoe(new Lance(joao,1000.0));
+				
+		//acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		//validacao
+		assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(1000.0, leiloeiro.getMenorLance(), 0.00001);
+	
+	}
+
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		
+		//cenario
+		Usuario joao = new Usuario("Joao");
+		Usuario maria = new Usuario("Maria");
+
+	
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+		
+		leilao.propoe(new Lance(joao,100.0));
+		leilao.propoe(new Lance(maria,200.0));
+		leilao.propoe(new Lance(joao,300.0));
+		leilao.propoe(new Lance(maria,400.0));
+				
+		//acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		//validacao
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		assertEquals(3, maiores.size());
+		assertEquals(400.0, maiores.get(0).getValor(), 0.0001);
+		assertEquals(300.0, maiores.get(1).getValor(), 0.0001);
+		assertEquals(200.0, maiores.get(2).getValor(), 0.0001);
+
+	}
+	
 }
