66a6c039e8c7e988e2827339ea2832bc24bc8273
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-04-03 11:03:57 -0300

    Aula 2.2

--- src/test/java/br/com/gft/istemaleilao/service/EncerradorDeLeilaoTest.java
+++ src/test/java/br/com/gft/istemaleilao/service/EncerradorDeLeilaoTest.java
@@ -3,6 +3,8 @@ package br.com.gft.istemaleilao.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -36,6 +38,23 @@ public class EncerradorDeLeilaoTest {
 				assertEquals(2, encerrador.getTotalEncerrados());
 				assertTrue(leilao1.isEncerrado());
 				assertTrue(leilao2 .isEncerrado());	
+	}
+	
+	@Test
+	public void deveAtualizarLeiloesEncerrados() throws Exception {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(199, 1, 20);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("TV 4K").naData(antiga).constroi();
+		
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1));
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+		encerrador.encerra();
+		
+		verify(daoFalso, times(1)).atualiza(leilao1);
 	}
 
 }
