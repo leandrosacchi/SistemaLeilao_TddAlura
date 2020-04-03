ca215354f08b33ca381cddd4487a646e22e58062
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-04-03 09:43:55 -0300

    Aula 2.1
    Inicio do Módulo 2: Testando comportamento através de mocks.

---
+++ src/main/java/br/com/gft/istemaleilao/dao/LeilaoDaoFalso.java
@@ -0,0 +1,38 @@
+package br.com.gft.istemaleilao.dao;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.gft.istemaleilao.domain.Leilao;
+
+public class LeilaoDaoFalso {
+
+	private static List<Leilao> leiloes = new ArrayList<Leilao>();;
+	
+	public void salva(Leilao leilao) {
+		leiloes.add(leilao);
+	}
+
+	public List<Leilao> encerrados() {
+		
+		List<Leilao> filtrados = new ArrayList<Leilao>();
+		for(Leilao leilao : leiloes) {
+			if(leilao.isEncerrado()) filtrados.add(leilao);
+		}
+
+		return filtrados;
+	}
+	
+	public List<Leilao> correntes() {
+		
+		List<Leilao> filtrados = new ArrayList<Leilao>();
+		for(Leilao leilao : leiloes) {
+			if(!leilao.isEncerrado()) filtrados.add(leilao);
+		}
+
+		return filtrados;
+	}
+	
+	public void atualiza(Leilao leilao) { /* faz nada! */ }
+}
---
+++ src/test/java/br/com/gft/istemaleilao/service/EncerradorDeLeilaoTest.java
@@ -0,0 +1,41 @@
+package br.com.gft.istemaleilao.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.gft.istemaleilao.builder.CriadorDeLeilao;
+import br.com.gft.istemaleilao.dao.LeilaoDao;
+import br.com.gft.istemaleilao.domain.Leilao;
+
+public class EncerradorDeLeilaoTest {
+	
+	@Test
+	public void deveEncerrarLeiloesQueComecaramUmaSemanaAntes() throws Exception {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 1, 20);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("Tv 4K").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
+		
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		
+		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+				encerrador.encerra();
+				
+				assertEquals(2, encerrador.getTotalEncerrados());
+				assertTrue(leilao1.isEncerrado());
+				assertTrue(leilao2 .isEncerrado());	
+	}
+
+}
